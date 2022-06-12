1d832ec3209d15d706cca61284dc55b7f883221a
finaleshot2016 <103410982+finaleshot2016@users.noreply.github.com>
2022-06-12 21:50:55 +0800

    Update Hero.jsx

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -18,8 +18,8 @@ const Hero = () => {
         Jirussic Park is a community-focused, decentralized memecoin on Binance Smart Chain with rewards in BUSD for holding $JP
         </p>
         <div>
-          <button className='m-2 bg-white hover:bg-pink-700 text-black font-bold py-2 px-4 rounded'>Buy</button>
-          <button className='m-2 bg-white hover:bg-pink-700 text-black font-bold py-2 px-4 rounded'>Chart</button>
+          <button className='m-2 bg-white hover:bg-pink-700 text-black font-bold py-2 px-4 rounded'><a href="https://pancakeswap.finance/swap?outputCurrency=0x7afbb79C338528841FaD4Fba93e45dcEaB7aa1dB">Buy</a></button>
+          <button className='m-2 bg-white hover:bg-pink-700 text-black font-bold py-2 px-4 rounded'><a href="https://poocoin.app/tokens/0x7afbb79c338528841fad4fba93e45dceab7aa1db">Chart</a></button>
         </div>
       </div>
       <div>
