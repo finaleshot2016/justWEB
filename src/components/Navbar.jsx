62302dc3451b6337b61eadca89d79cce362b64af
finaleshot2016 <103410982+finaleshot2016@users.noreply.github.com>
2022-06-12 21:51:27 +0800

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -20,7 +20,7 @@ const Navbar = () => {
             <li><a href="#tokenomics">Tokenomics</a></li>
             <li><a href="https://t.me/JirussicParkPortal">Telegram</a></li>
             <li><a href="https://twitter.com/JirussicParkBSC">Twitter</a></li>
-            <button className='m-2 bg-white hover:bg-pink-700 text-black font-bold py-2 px-4 rounded'>Buy on Pancakeswap</button>
+            <button className='m-2 bg-white hover:bg-pink-700 text-black font-bold py-2 px-4 rounded'><a href="https://pancakeswap.finance/swap?outputCurrency=0x7afbb79C338528841FaD4Fba93e45dcEaB7aa1dB">Buy on Pancakeswap</a></button>
           </ul>
          </div>
 
