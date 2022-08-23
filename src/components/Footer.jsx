327a4cbab3914a816e54217afc8879d9d35f0c77
FINALESHOT2016 <103410982+finaleshot2016@users.noreply.github.com>
2022-08-24 01:21:53 +0800

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -8,7 +8,7 @@ const Footer = () => {
     <div className='bg-black text-white justify-center items-center py-20'>
         <div className='flex justify-center grid-rows-1 w-full py-2'>
             <div>
-<<<<<<< HEAD
+
             <a href="https://twitter.com"><img className='max-w-[100px]' src={terminal} alt="https://twitter.com/" /></a>
 
             <img className='max-w-[100px]' src={terminal1} alt="/" />
@@ -19,14 +19,13 @@ const Footer = () => {
  <p className='text-xl text-center mt-5 py-10 text-white'>
 Copyright © 2022 JUST APE
         </p>
-=======
+
                 <h3 className='font-bold text-xl'><a href="https://t.me/THEDOGESTYLE_DC">Telegram</a></h3>
             </div>
             <div>
                 <h3 className='font-bold text-xl'><a href="www.twitter.com/THEDOGESTYLE">Twitter</a></h3>
             </div>
         </div>
->>>>>>> 4eba0e75502138b64dc6936526263f4b9ca96001
     </div>
   )
 }
