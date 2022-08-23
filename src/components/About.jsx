790665806e7b437c400d7505d169d789b6776819
FINALESHOT2016 <103410982+finaleshot2016@users.noreply.github.com>
2022-08-24 01:40:44 +0800

    Update About.jsx

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -12,11 +12,7 @@ const About = () => {
           <div className='grid sm:grid-cols-2 lg:grid-cols-4 gap-4'>
 
             {/* Card */}
-
-            <AboutCard heading='Phase 1' text='Contract Deployment, Uniswap Listing, Marketing Campaign & 200 Holders' />
-
             <AboutCard heading='Phase 1' text='Contract Deployment, Dogeswap Listing, Marketing Campaign & 200 Holders' />
-
             <AboutCard heading='Phase 2' text='Website Live, More Marketing Campaign, CMC and CG Listing & 500 Holders.' />
             <AboutCard heading='Phase 3' text='Website Upgrade, CEX Listing, Dashboard, Whitepaper, More Marketing Campaign & 1,000 Holders.' />
             <AboutCard heading='Phase 4' text='Usecase Reveal & 5,000 Holders.' />
